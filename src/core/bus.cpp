be0d87e3cd481b41e54528043ca3005fe32cb504
MiSzostak <szostak.michael@gmail.com>
2020-06-27 18:27:57 +0200

    bus: refactor load and store functions

--- src/core/bus.cpp
+++ src/core/bus.cpp
@@ -86,171 +86,120 @@ bool BUS::LoadPsExe(const std::string& path) {
     return true;
 }
 
+static constexpr bool InArea(u32 start, u32 length, u32 address) {
+    return address >= start && address < start + length;
+}
+
 template <typename ValueType>
 ValueType BUS::Load(u32 address) {
     static_assert(std::is_same<ValueType, u32>::value || std::is_same<ValueType, u16>::value ||
                   std::is_same<ValueType, u8>::value);
-    u32 masked_address = MaskRegion(address);
-    u32 mask = (masked_address & 0xFF000000) >> 24;
+    const u32 masked_addr = MaskRegion(address);
 
-    switch (mask) {
-        case 0x00:  // RAM
-            Assert(masked_address < RAM_SIZE);
-            return *reinterpret_cast<ValueType*>(ram.data() + masked_address);
-        case 0x1F:
-            switch ((masked_address & 0xF00000) >> 20) {
-                case 0x0:
-                case 0x1:
-                case 0x2:
-                case 0x3:
-                case 0x4:
-                case 0x5:
-                case 0x6:
-                case 0x7:  // Expansion Region 1
-                    LOG_WARN << fmt::format("Tried to load from Expansion Region 1 [0x{:08X}]", address);
-                    return 0xFF;
-                case 0x8:
-                    switch ((masked_address & 0xF000) >> 12) {
-                        case 0x0: {  // Scratchpad
-                            u32 rel_address = masked_address - 0x1F800000;
-                            Assert(rel_address < SCRATCH_SIZE);
-                            //LOG_DEBUG << fmt::format("Load from Scratchpad [0x{:08X}]", address);
-                            return *reinterpret_cast<ValueType*>(scratchpad.data() + rel_address);
-                        }
-                        case 0x1: {  // IO Ports
-                            u32 rel_address = masked_address - 0x1F801000;
-                            Assert(rel_address < 1024 * 8);
-                            if (masked_address == 0x1F801070) return interrupt->LoadStat();
-                            if (masked_address == 0x1F801074) return interrupt->LoadMask();
-                            if (masked_address == 0x1F801110) return 0; // Timer 1 (horizontal retrace)
-                            if (masked_address == 0x1F801810) return gpu->gpu_read; // GPUREAD
-                            if (masked_address == 0x1F801814) return (ValueType) gpu->ReadStat(); // GPUSTAT
-                            if (masked_address >= 0x1F801080 && masked_address <= 0x1F8010F4) return (ValueType) dma->Load(rel_address - 0x80); // DMA
-                            if (masked_address >= 0x1F801C00 && masked_address <= 0x1F801E80) return 0; // SPU
-                            if (masked_address >= 0x1F801040 && masked_address <= 0x1F80104E) return 0; // Joypad
-                            Panic("Tried to load from IO Ports [0x%08X]", address);
-                            break;
-                        }
-                        case 0x2: {  // Expansion Region 2
-                            u32 rel_address = masked_address - 0x1F802000;
-                            Assert(rel_address < 1024 * 8);
-                            Panic("Tried to load from Expansion Region 2 [0x%08X]", address);
-                            break;
-                        }
-                        default:
-                            Panic("Tried to load from invalid address range [0x%08X]", address);
-                    }
-                    break;
-                case 0xA: {  // Expansion Region 3
-                    u32 rel_address = masked_address - 0x1FA00000;
-                    Assert(rel_address < 1024 * 2048);
-                    Panic("Tried to load from Expansion Region 3 [0x%08X]", address);
-                    break;
-                }
-                case 0xC: {  // BIOS
-                    // printf("Load from BIOS at address 0x%08X\n", address);
-                    u32 rel_address = masked_address - 0x1FC00000;
-                    Assert(rel_address < BIOS_FILE_SIZE);
-                    return *reinterpret_cast<ValueType*>(bios.data() + rel_address);
-                }
-                default:
-                    Panic("Tried to load from invalid address range [0x%08X]", address);
-            }
-        case 0xFF: {  // Cache Control
-            u32 rel_address = masked_address - 0xFFFE0000;
-            Assert(rel_address < 512);
-            Panic("Tried to load from Cache Control [0x%08X]", address);
-            break;
+    // RAM
+    if (InArea(0x00000000, RAM_SIZE, masked_addr)) return *reinterpret_cast<ValueType*>(ram.data() + masked_addr);
+    // Scratchpad
+    if (InArea(0x1F800000, SCRATCH_SIZE, masked_addr))
+        return *reinterpret_cast<ValueType*>(scratchpad.data() + (masked_addr - 0x1F800000));
+    // IO Ports
+    if (InArea(0x1F801000, 8 * 1024, masked_addr)) {
+        switch (masked_addr) {
+            case(0x1F801070): return interrupt->LoadStat();
+            case(0x1F801074): return interrupt->LoadMask();
+            case(0x1F801110): return 0; // Timer 1 (horizontal retrace)
+            case(0x1F801810): return gpu->gpu_read; // GPUREAD
+            case(0x1F801814): return (ValueType) gpu->ReadStat(); // GPUSTAT
         }
-        default:
-            Panic("Tried to load from invalid address range [0x%08X]", address);
+        if (InArea(0x1F801080, 120, masked_addr)) return (ValueType) dma->Load(masked_addr - 0x1F801080);
+        if (InArea(0x1F801C00, 644, masked_addr)) return 0; // SPU
+        if (InArea(0x1F801040, 16, masked_addr)) return 0;  // Joypad
+        Panic("Tried to load from IO Ports [0x%08X]", address);
+    }
+    // BIOS
+    if (InArea(0x1FC00000, BIOS_FILE_SIZE, masked_addr))
+        return *reinterpret_cast<ValueType*>(bios.data() + (masked_addr - 0x1FC00000));
+    // Cache Control
+    if (InArea(0xFFFE0000, 512, masked_addr)) Panic("Tried to load from Cache Control [0x%08X]", address);
+    // Expansion Region 1
+    if (InArea(0x1F000000, 8192 * 1024, masked_addr)) {
+        LOG_WARN << fmt::format("Tried to load from Expansion Region 1 [0x{:08X}]", address);
+        return 0xFF;
+    }
+    // Expansion Region 2
+    if (InArea(0x1F802000, 8 * 1024, masked_addr)) {
+        Panic("Tried to load from Expansion Region 2 [0x%08X]", address);
+    }
+    // Expansion Region 3
+    if (InArea(0x1FA00000, 2048 * 1024, masked_addr)) {
+        Panic("Tried to load from Expansion Region 3 [0x%08X]", address);
     }
 
-    // unreachable
+    Panic("Tried to load from invalid address [0x%08X]", address);
+    return 0;
 }
 
 template <typename Value>
 void BUS::Store(u32 address, Value value) {
     static_assert(std::is_same<decltype(value), u32>::value || std::is_same<decltype(value), u16>::value ||
                   std::is_same<decltype(value), u8>::value);
-    u32 masked_address = MaskRegion(address);
-    u32 mask = (masked_address & 0xFF000000) >> 24;
+    const u32 masked_addr = MaskRegion(address);
 
-    switch (mask) {
-        case 0x00:  // RAM
-            Assert(masked_address < RAM_SIZE);
-            std::copy(reinterpret_cast<u8*>(&value), reinterpret_cast<u8*>(&value) + sizeof(value),
-                      ram.data() + masked_address);
-            break;
-        case 0x1F:
-            switch ((masked_address & 0xF00000) >> 20) {
-                case 0x0:
-                case 0x1:
-                case 0x2:
-                case 0x3:
-                case 0x4:
-                case 0x5:
-                case 0x6:
-                case 0x7:  // Expansion Region 1
-                    Panic("Tried to store in Expansion Region 1 [0x%08X]", address);
-                case 0x8:
-                    switch ((masked_address & 0xF000) >> 12) {
-                        case 0x0: {  // Scratchpad
-                            u32 rel_address = masked_address - 0x1F800000;
-                            Assert(rel_address < SCRATCH_SIZE);
-                            //LOG_DEBUG << fmt::format("Store in Scratchpad [0x{:08X}]", address);
-                            std::copy(reinterpret_cast<u8*>(&value), reinterpret_cast<u8*>(&value) + sizeof(value),
-                                      scratchpad.data() + rel_address);
-                            break;
-                        }
-                        case 0x1: {  // IO Ports
-                            u32 rel_address = masked_address - 0x1F801000;
-                            Assert(rel_address < 1024 * 8);
-                            if (masked_address == 0x1F801070) interrupt->StoreStat(value);
-                            else if (masked_address == 0x1F801074) interrupt->StoreMask(value);
-                            else if (masked_address == 0x1F801810) gpu->SendGP0Cmd(value);
-                            else if (masked_address == 0x1F801814) gpu->SendGP1Cmd(value);
-                            else if (masked_address >= 0x1F801080 && masked_address <= 0x1F8010F4) dma->Store(rel_address - 0x80, value); // DMA
-                            else if (masked_address >= 0x1F801C00 && masked_address <= 0x1F801E80) break; // SPU
-                            else if (masked_address >= 0x1F801800 && masked_address <= 0x1F801803) { Panic("CDROM"); }
-                            else LOG_WARN << fmt::format("Store<{}> call to IO Ports [0x{:X} @ 0x{:08X}] - Ignored", sizeof(Value)*8, value, address);
-                            break;
-                        }
-                        case 0x2: {  // Expansion Region 2
-                            u32 rel_address = masked_address - 0x1F802000;
-                            Assert(rel_address < 1024 * 8);
-                            LOG_WARN << fmt::format("Tried to store in Expansion Region 2 [0x{:X} @ 0x{:08X}] - Ignored", value, address);
-                            break;
-                        }
-                        default:
-                            Panic("Tried to store in invalid address range [0x%08X]", address);
-                    }
-                    break;
-                case 0xA: {  // Expansion Region 3
-                    u32 rel_address = masked_address - 0x1FA00000;
-                    Assert(rel_address < 1024 * 2048);
-                    Panic("Tried to store in Expansion Region 3 [0x%08X]", address);
-                    break;
-                }
-                case 0xC: {  // BIOS
-                    LOG_WARN << fmt::format("Tried to store value in BIOS address range [0x{:08X}] - Ignored", address);
-                    break;
-                }
-                default:
-                    Panic("Tried to store in invalid address range [0x%08X]", address);
-            }
-            break;
-        case 0xFF: {  // Cache Control
-            u32 rel_address = masked_address - 0xFFFE0000;
-            Assert(rel_address < 512);
-            LOG_WARN << fmt::format("Store call to Cache Control [0x{:X} @ 0x{:08X}] - Ignored", value, address);
-            break;
+    // RAM
+    if (InArea(0x00000000, RAM_SIZE, masked_addr)) {
+        std::copy(reinterpret_cast<u8*>(&value), reinterpret_cast<u8*>(&value) + sizeof(value),
+                  ram.data() + masked_addr);
+        return;
+    }
+    // Scratchpad
+    if (InArea(0x1F800000, SCRATCH_SIZE, masked_addr)) {
+        std::copy(reinterpret_cast<u8*>(&value), reinterpret_cast<u8*>(&value) + sizeof(value),
+                  scratchpad.data() + (masked_addr - 0x1F800000));
+        return;
+    }
+    // IO Ports
+    if (InArea(0x1F801000, 8 * 1024, masked_addr)) {
+        switch (masked_addr) {
+            case(0x1F801070): interrupt->StoreStat(value); return;
+            case(0x1F801074): interrupt->StoreMask(value); return;
+            case(0x1F801810): gpu->SendGP0Cmd(value); return;
+            case(0x1F801814): gpu->SendGP1Cmd(value); return;
         }
-        default:
-            Panic("Tried to store in invalid address range [0x%08X]", address);
+        if (InArea(0x1F801080, 120, masked_addr)) {
+            dma->Store(masked_addr - 0x1F801080, value);
+            return;
+        }
+        if (InArea(0x1F801C00, 644, masked_addr)) return; // SPU
+        if (InArea(0x1F801800, 4, masked_addr)) Panic("CDROM");  // CDROM
+        LOG_WARN << fmt::format("Store<{}> call to IO Ports [0x{:X} @ 0x{:08X}] - Ignored",
+                                sizeof(Value) * 8, value, address);
+        return;
+    }
+    // BIOS
+    if (InArea(0x1FC00000, BIOS_FILE_SIZE, masked_addr)) {
+        LOG_WARN << fmt::format("Tried to store value in BIOS address range [0x{:08X}] - Ignored", address);
+        return;
+    }
+    // Cache Control
+    if (InArea(0xFFFE0000, 512, masked_addr)) {
+        LOG_WARN << fmt::format("Store call to Cache Control [0x{:X} @ 0x{:08X}] - Ignored", value, address);
+        return;
+    }
+    // Expansion Region 1
+    if (InArea(0x1F000000, 8192 * 1024, masked_addr)) {
+        Panic("Tried to store in Expansion Region 1 [0x%08X]", address);
+    }
+    // Expansion Region 2
+    if (InArea(0x1F802000, 8 * 1024, masked_addr)) {
+        LOG_WARN << fmt::format("Tried to store in Expansion Region 2 [0x{:X} @ 0x{:08X}] - Ignored", value, address);
+        return;
+    }
+    // Expansion Region 3
+    if (InArea(0x1FA00000, 2048 * 1024, masked_addr)) {
+        Panic("Tried to store in Expansion Region 3 [0x%08X]", address);
     }
 
-    // unreachable
+    Panic("Tried to store in invalid address [0x%08X]", address);
+    return;
 }
 
 void BUS::Reset() {
